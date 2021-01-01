3e9da7ab3086b4fbfa657bfead70a6ce8f5057d3
Eder Ramirez <eder.ramirez87@gmail.com>
2021-01-01 14:40:48 -0600

    AddCategory component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,31 @@
+import React from "react";
+import { useState } from "react";
+import AddCategory from "./components/AddCategory";
+
 const GifExpertApp = () => {
-  return ( 
+  const [categories, setCategories] = useState([
+    "One punch",
+    "Samurai X",
+    "Dragon ball",
+  ]);
+
+  /* const handleAdd = (el) => {
+    setCategories([...categories, el]);
+  }; */
+
+  return (
     <div>
       <h2>GifExpertApp</h2>
-      <hr/>
+      <AddCategory />
+      <hr />
+
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </div>
-   );
-}
- 
+  );
+};
+
 export default GifExpertApp;
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const AddCategory = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <>
+      <h1>{inputValue}</h1>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+    </>
+  );
+};
+
+export default AddCategory;
