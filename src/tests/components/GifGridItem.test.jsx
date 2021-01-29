c25d4e5942561f00ed3f6df07f470e0a1a7757df
Eder Ramirez <eder.ramirez87@gmail.com>
2021-01-28 20:36:41 -0600

    GifGridItem tests

--- src/tests/components/GifGridItem.test.jsx
+++ src/tests/components/GifGridItem.test.jsx
@@ -4,10 +4,29 @@ import GifGridItem from "../../components/GifGridItem";
 import toJson from "enzyme-to-json";
 
 describe("GidGridItem.jsx", () => {
+  const title = "title";
+  const url = "https://url.com/image.jpg";
+  const wrapper = shallow(<GifGridItem title={title} url={url} />);
   it("Component should display correctly", () => {
-    const title = "title";
-    const url = "https://url.com/image.jpg";
-    const wrapper = shallow(<GifGridItem title={title} url={url} />);
     expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it("Should have a paragraph with title", () => {
+    const p = wrapper.find("p");
+    expect(p.text().trim()).toBe(title);
+  });
+
+  it("Should have the image equal to url and props", () => {
+    const img = wrapper.find("img");
+    // console.log(img.props("src"));
+    expect(img.prop("src")).toBe(url);
+    expect(img.prop("alt")).toBe(title);
+  });
+
+  it("should have animate__fadeIn class", () => {
+    const div = wrapper.find("div");
+    expect(div.prop("className")).toBe(
+      "card animate__animated animate__fadeIn"
+    );
   });
 });
